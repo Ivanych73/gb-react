bfd5b50081689c2a9d60cee559913d7346b54011
Kirill Novichkov <kin190173@gmail.com>
2021-07-23 15:35:11 +0500

    lesson3 homework done

--- src/components/message/message.js
+++ src/components/message/message.js
@@ -1,15 +1,19 @@
 import './message.css';
 
 function Message(props) {
+    let messageStyle;
+    if (props.message.author === 'user1') {
+        messageStyle = 'message-author'
+    } else {
+        messageStyle = 'message-other'
+    }
     return (
-        <div className='message'>
+        <div className = {messageStyle} >
             <div className= 'author'>
-                <p>Author:</p>
                 <p>{props.message.author}</p>
             </div>
             <div className = 'text'>
-            <p>Message:</p>
-            <p>{props.message.text}</p>
+                <p>{props.message.text}</p>
             </div>
         </div>
     )
--- src/components/messageList/messageList.js
+++ src/components/messageList/messageList.js
@@ -3,8 +3,8 @@ import Message from '../message/message'
 function MessageList(props) {
     return (
         props.messages.map(
-            (message) =>
-            <Message message={message}/>
+            (message, index) =>
+            <Message key={index} message={message}/>
         )
     )
 }
